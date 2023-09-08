0db477e4a8ea7119836be7ca9acda4206d903aea
Joshbuild10 <obayomijo@gmail.com>
2023-09-08 19:53:00 +0100

    Add full query functionality

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -8,9 +8,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(os.environ['SQLDB'], 'questions.db'),
+        DATABASE=os.path.join(os.environ['QDB'], 'questions.db'),
     )
-    app.config['DATABASE'] = os.path.join(os.environ['SQLDB'], 'questions.db')
+    app.config['DATABASE'] = os.path.join(os.environ['QDB'], 'questions.db')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
